ce01a3e7f4b1bed2d6f8f2fbde5b03c4e5bcd8ec
Your Name <you@example.com>
2018-05-17 00:48:52 +0800

    make kafkagroup

--- yxsdb/kafkautils.go
+++ yxsdb/kafkautils.go
@@ -7,16 +7,30 @@ import (
 	"github.com/Shopify/sarama"
 )
 
-var (
-	producerMap     = make(map[string]*sarama.AsyncProducer)
-	producerMapLock = make(chan bool, 1)
+type KafkaGroup struct {
+	Connection      string
+	producerMap     map[string]*sarama.AsyncProducer
+	producerMapLock chan bool
+	consumerMap     map[string]*sarama.Consumer
+	consumerMapLock chan bool
+}
 
-	consumerMap     = make(map[string]*sarama.Consumer)
-	consumerMapLock = make(chan bool, 1)
-)
+type KafkaNode struct {
+	ID string
+}
 
-func createTopic(topicName string) error {
-	broker := sarama.NewBroker("localhost:9092")
+func InitKafkaGroup(connection string) *KafkaGroup {
+	kgroup := KafkaGroup{Connection: connection,
+		producerMap:     make(map[string]*sarama.AsyncProducer),
+		producerMapLock: make(chan bool, 1),
+		consumerMap:     make(map[string]*sarama.Consumer),
+		consumerMapLock: make(chan bool, 1),
+	}
+	return &kgroup
+}
+
+func (group *KafkaGroup) CreateTopic(topicName string) error {
+	broker := sarama.NewBroker(group.Connection)
 	err := broker.Open(nil)
 	if err != nil {
 		panic(err)
@@ -28,73 +42,69 @@ func createTopic(topicName string) error {
 		_ = broker.Close()
 		return err
 	}
-
-	// log.Printf("create Topic is %v", response.Topics)
-	// log.Println("There are", len(response.Topics), "topics active in the cluster.")
-
 	if err = broker.Close(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func getProducer(name string) (*sarama.AsyncProducer, error) {
-	producerMapLock <- true
+func (group *KafkaGroup) GetProducer(name string) (*sarama.AsyncProducer, error) {
+	group.producerMapLock <- true
 	defer func() {
-		<-producerMapLock
+		<-group.producerMapLock
 	}()
 
-	producer, ok := producerMap[name]
+	producer, ok := group.producerMap[name]
 	if !ok {
-		producer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, nil)
+		producer, err := sarama.NewAsyncProducer([]string{group.Connection}, nil)
 		if err != nil {
 			return nil, err
 		}
-		producerMap[name] = &producer
+		group.producerMap[name] = &producer
 		return &producer, nil
 	} else {
 		return producer, nil
 	}
 }
 
-func getConsumer(name string) *sarama.Consumer {
-	producerMapLock <- true
+func (group *KafkaGroup) GetConsumer(name string) *sarama.Consumer {
+	group.producerMapLock <- true
 	defer func() {
-		<-producerMapLock
+		<-group.producerMapLock
 	}()
 
-	consumer, ok := consumerMap[name]
+	consumer, ok := group.consumerMap[name]
 	if !ok {
-		consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, nil)
+		consumer, err := sarama.NewConsumer([]string{group.Connection}, nil)
 		if err != nil {
 			panic(err)
 		}
-		consumerMap[name] = &consumer
+		group.consumerMap[name] = &consumer
 		return &consumer
 	} else {
 		return consumer
 	}
 }
 
-func sendMessage(producer *sarama.AsyncProducer, topic string, key string, value string) {
+func (group *KafkaGroup) SendMessage(producer *sarama.AsyncProducer, topic string, key string, value string) {
 	// log.Printf("send msg to %s", topic)
 	(*producer).Input() <- &sarama.ProducerMessage{Topic: topic, Key: sarama.StringEncoder(key), Value: sarama.StringEncoder(value)}
 }
 
-func doTopicCreate(level1 int, level2 int) {
+func (group *KafkaGroup) BatchTopicCreate(maxLevel1 int, maxLevel2 int) {
 
-	topic := fmt.Sprintf("nq_%v_%v", level1, level2)
-	createTopic(topic)
+	topic := group.GetSubTopicName("request", maxLevel1, maxLevel2)
+	group.CreateTopic(topic)
 
-	for x := 0; x < level1; x++ {
-		for y := 0; y < level2; y++ {
-			topic := fmt.Sprintf("nq_%v_%v_%v_%v", level1, level2, x, y)
-			createTopic(topic)
+	for x := 0; x < maxLevel1; x++ {
+		for y := 0; y < maxLevel2; y++ {
+			topicName := group.GetNodeTopicName("request", maxLevel1, maxLevel2, x, y)
+			group.CreateTopic(topicName)
 		}
 	}
 }
 
-func handleMessage(consumer *sarama.Consumer, topic string, handler func(msg *sarama.ConsumerMessage)) {
+func (group *KafkaGroup) HandleMessage(consumer *sarama.Consumer, topic string, handler func(msg *sarama.ConsumerMessage)) {
 	go func() {
 		partitionConsumer, err := (*consumer).ConsumePartition(topic, 0, sarama.OffsetOldest)
 		if err != nil {
@@ -111,41 +121,41 @@ func handleMessage(consumer *sarama.Consumer, topic string, handler func(msg *sa
 	}()
 }
 
-func addNode(maxLevel1, maxLevel2, level1, level2 int) {
-	nodeName := getNodeTopicName("node", maxLevel1, maxLevel2, level1, level2)
-	topicName := getNodeTopicName("request", maxLevel1, maxLevel2, level1, level2)
-	subTopicName := getSubTopicName("request", maxLevel1, maxLevel2)
-	subResponseTopicName := getSubTopicName("response", maxLevel1, maxLevel2)
+func (group *KafkaGroup) AddNode(maxLevel1, maxLevel2, level1, level2 int) {
+	nodeName := group.GetNodeTopicName("node", maxLevel1, maxLevel2, level1, level2)
+	topicName := group.GetNodeTopicName("request", maxLevel1, maxLevel2, level1, level2)
+	subTopicName := group.GetSubTopicName("request", maxLevel1, maxLevel2)
+	subResponseTopicName := group.GetSubTopicName("response", maxLevel1, maxLevel2)
 
-	nodeConsumer := getConsumer(nodeName)
+	nodeConsumer := group.GetConsumer(nodeName)
 	handler := func(msg *sarama.ConsumerMessage) {
-		log.Printf("Node %s get message,%s=%s, offset=%v", nodeName, msg.Key, msg.Value, msg.Offset)
-		nodeProducer, _ := getProducer(nodeName)
-		sendMessage(nodeProducer, subResponseTopicName, fmt.Sprintf("NODE=%s, done!!!", nodeName), "value")
+		log.Printf("Node %s get message,%s=%s, offset=%v, topic=%s", nodeName, msg.Key, msg.Value, msg.Offset, msg.Topic)
+		nodeProducer, _ := group.GetProducer(nodeName)
+		group.SendMessage(nodeProducer, subResponseTopicName, fmt.Sprintf("NODE=%s, done!!!", nodeName), "value")
 	}
-	handleMessage(nodeConsumer, topicName, handler)
-	handleMessage(nodeConsumer, subTopicName, handler)
+	group.HandleMessage(nodeConsumer, topicName, handler)
+	group.HandleMessage(nodeConsumer, subTopicName, handler)
 
 }
 
-func addAllNode(maxLevel1, maxLevel2 int) {
+func (group *KafkaGroup) AddAllNode(maxLevel1, maxLevel2 int) {
 	for x := 0; x < maxLevel1; x++ {
 		for y := 0; y < maxLevel2; y++ {
-			addNode(maxLevel1, maxLevel2, x, y)
+			group.AddNode(maxLevel1, maxLevel2, x, y)
 		}
 	}
 }
 
-func getNodeTopicNameByID(topicPrefix string, id int64, maxLevel1, maxLevel2 int) string {
+func (group *KafkaGroup) GetNodeTopicNameByID(topicPrefix string, id int64, maxLevel1, maxLevel2 int) string {
 	level1 := id % int64(maxLevel1)
 	level2 := id % int64(maxLevel2)
-	return getNodeTopicName(topicPrefix, maxLevel1, maxLevel2, int(level1), int(level2))
+	return group.GetNodeTopicName(topicPrefix, maxLevel1, maxLevel2, int(level1), int(level2))
 }
 
-func getNodeTopicName(topicPrefix string, maxLevel1, maxLevel2, level1, level2 int) string {
+func (group *KafkaGroup) GetNodeTopicName(topicPrefix string, maxLevel1, maxLevel2, level1, level2 int) string {
 	return fmt.Sprintf("%s_%v_%v_%v_%v", topicPrefix, maxLevel1, maxLevel2, level1, level2)
 }
 
-func getSubTopicName(topicPrefix string, maxLevel1, maxLevel2 int) string {
+func (group *KafkaGroup) GetSubTopicName(topicPrefix string, maxLevel1, maxLevel2 int) string {
 	return fmt.Sprintf("%s_%v_%v", topicPrefix, maxLevel1, maxLevel2)
 }
--- yxsdb/kafkautils_test.go
+++ yxsdb/kafkautils_test.go
@@ -11,32 +11,32 @@ import (
 func TestKafka(t *testing.T) {
 	log.Printf("begin")
 
+	kgroup := InitKafkaGroup("localhost:9092")
+
 	maxLevel1 := 3
 	maxLevel2 := 5
-	doTopicCreate(3, 5)
-	createTopic("dbmeta")
-	createTopic("nodemeta")
+	kgroup.BatchTopicCreate(3, 5)
+	kgroup.CreateTopic("dbmeta")
+	kgroup.CreateTopic("nodemeta")
 
-	// doProducer()
-	// doConsumer()
-	addAllNode(maxLevel1, maxLevel2)
+	kgroup.AddAllNode(maxLevel1, maxLevel2)
 
-	consumer := getConsumer("master")
+	consumer := kgroup.GetConsumer("master")
 	masterHandler := func(msg *sarama.ConsumerMessage) {
 		log.Printf("MASTER Node get message, %s=%s, offset=%v, topic=%s", msg.Key, msg.Value, msg.Offset, msg.Topic)
 	}
-	handleMessage(consumer, getSubTopicName("response", maxLevel1, maxLevel2), masterHandler)
+	kgroup.HandleMessage(consumer, kgroup.GetSubTopicName("response", maxLevel1, maxLevel2), masterHandler)
 
-	masterP, _ := getProducer("master")
+	masterP, _ := kgroup.GetProducer("master")
 	log.Printf("masterP is %v", masterP)
 
 	for i := 0; i < 1; i++ {
-		sendMessage(masterP, getNodeTopicNameByID("request", int64(i), maxLevel1, maxLevel2), "1", "create table if not exists _0(_0,_1,_2,_3)")
+		kgroup.SendMessage(masterP, kgroup.GetNodeTopicNameByID("request", int64(i), maxLevel1, maxLevel2), "1", "create table if not exists _0(_0,_1,_2,_3)")
 		time.Sleep(50 * time.Millisecond)
 	}
 
 	for i := 0; i < 1; i++ {
-		sendMessage(masterP, getSubTopicName("request", maxLevel1, maxLevel2), "1", "create table if not exists _0(_0,_1,_2,_3)")
+		kgroup.SendMessage(masterP, kgroup.GetSubTopicName("request", maxLevel1, maxLevel2), "1", "create table if not exists _0(_0,_1,_2,_3)")
 		time.Sleep(50 * time.Millisecond)
 	}
 
